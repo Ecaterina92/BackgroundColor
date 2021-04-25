31b9d002e4020f8117894ec40b22f5fab382640a
Ecaterina Pihurov <ecaterinaoprea92@gmail.com>
2021-04-25 17:40:30 +0300

    First commit

---
+++ change.js
@@ -0,0 +1,26 @@
+function myFunction() {
+let randomColor = Math.floor(Math.random() * 8);
+switch(randomColor) {
+case 1:
+document.body.style.backgroundColor = "yellow";
+break;
+case 2: 
+document.body.style.backgroundColor = "red";
+break;
+case 3:
+document.body.style.backgroundColor = "green";
+break;
+case 4:
+document.body.style.backgroundColor = "blue";  
+break;
+case 5:
+document.body.style.backgroundColor = "black";
+break;
+case 6:
+document.body.style.backgroundColor = "orange";
+break;
+case 7:
+document.body.style.backgroundColor = "pink";       
+break;
+}
+}
